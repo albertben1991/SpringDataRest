4e688eb471c9a8f3dce01aa8c3ce50a95decdc41
alber <alber@LAPTOP-1PC17HK7>
2019-05-10 22:08:13 +0200

    first commit

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'navin','java');
+insert into alien values (102,'omar','python');
+insert into alien values (103,'meet','Android');
+insert into alien values (104,'lal','java');
+insert into alien values (105,'mohit','Android');
